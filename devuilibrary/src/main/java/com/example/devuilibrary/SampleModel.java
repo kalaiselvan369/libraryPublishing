51d986c28e9690c43bbf6c36106e093a43ffd5be
kalaiselvan <kalaiselvan.1@tcs.com>
2016-06-19 19:54:35 +0530

    firstcommit

---
+++ devuilibrary/src/main/java/com/example/devuilibrary/SampleModel.java
@@ -0,0 +1,11 @@
+package com.example.devuilibrary;
+
+/**
+ * Created by Airbase on 19-Jun-16.
+ */
+public class SampleModel {
+
+    public static String getName() {
+        return "Android local library publishing";
+    }
+}
